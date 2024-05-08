384e72af1e50cc947110a0c1151276d111edc958
JulianaYS <u20201b782@upc.edu.pe>
2024-05-08 01:44:17 -0500

    feat: added appdatabase and my application

--- app/src/main/java/pe/edu/upc/eatsexplorer/feature_restaurant/data/remote/RestaurantResponse.kt
+++ app/src/main/java/pe/edu/upc/eatsexplorer/feature_restaurant/data/remote/RestaurantResponse.kt
@@ -5,6 +5,7 @@ package pe.edu.upc.eatsexplorer.feature_restaurant.data.remote
 typealias RestaurantsResponse = List<RestaurantResponse>
 //los datos que retorna el endpoint
 data class RestaurantResponse(
+    val id: Int,
     val title: String,
     val poster: String
 )
